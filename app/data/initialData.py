398980ed017c7211760c096f83d043591c2041f7
jeff.weisman <jweisman@deloitte.com>
2023-07-05 13:17:19 -0400

    update init data

---
+++ app/data/initialData.py
@@ -0,0 +1,13 @@
+from datetime import date
+
+DATASETS = [
+    {
+        "id": 1,
+        "name": "APIS",
+        "full_name": "Advance Passenger Information System",
+        "description":"APIS enhances border security by providing officers with pre-arrival and departure manifest data on all passengers and crew members"
+        "date_loaded": date(2002, 12, 31),
+        "owner": "CBP",
+        "classification": "UNCLASSIFIED",
+    },
+]
