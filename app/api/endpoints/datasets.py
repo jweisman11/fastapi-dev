81e397ee091bfdfd947474a6ba4f5c221a73b015
jeff.weisman <jweisman@deloitte.com>
2023-07-05 14:39:21 -0400

    ADD: metadata to health endpoint

--- app/api/endpoints/datasets.py
+++ app/api/endpoints/datasets.py
@@ -8,16 +8,24 @@ from data.initialData import DATASETS
 router = APIRouter()
 
 
-@router.get("/health", status_code=200)
-async def root() -> dict:
+@router.get(
+    path="/health",
+    response_model="",
+    status_code=status.HTTP_200_OK,
+    summary="Health Check",
+    description="TODO: Add longer description about what this API endpoint does",
+    response_description="TODO: Return health status of the dataset API endpoints",
+    deprecated=False,
+)
+async def health_check() -> dict:
     """
     Root GET
     """
 
-    return {"msg": "Hello, World!"}
+    return {"msg": "API is up and running"}
 
 
-# @api_router.get("/{dataset_id}", status_code=200, response_model=Recipe)
+# TODO: add response model
 @router.get("/{dataset_id}", status_code=status.HTTP_200_OK)
 async def fetch_dataset(*, dataset_id: int) -> dict:
     """
